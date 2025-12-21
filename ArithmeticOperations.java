f04fc004a93bf359ac39503025f9f2c8b4080ee0
arunothayar-oss <arunothayar@gmail.com>
2025-12-21 19:28:07 +0530

    for showing

---
+++ ArithmeticOperations.java
@@ -0,0 +1,18 @@
+class ArithmeticOperations
+{
+	public static void main(String[] args)
+	{
+	int a =20;
+	int b=6;
+	int addition=a+b;
+	int subtraction=a-b;
+	int multiplication=a*b;
+	int division=a/b;
+	int remainder=a%b;
+	System.out.println("Addition="+addition);
+	System.out.println("Subtraction="+subtraction);
+	System.out.println("Multiplication="+multiplication);
+	System.out.println("Division="+division);
+	System.out.println("Remainder="+remainder);
+	}
+}	
