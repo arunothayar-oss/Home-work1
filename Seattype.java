603eaf4efce589e202e24966e67815b931ac72f5
arunothayar-oss <arunothayar@gmail.com>
2025-12-26 13:19:13 +0530

    for showing

--- Seattype.java
+++ Seattype.java
@@ -11,19 +11,24 @@ class Seattype
 		switch(seat)
 		{
 			case 1:
-			System.out.println("General");
+			System.out.println("class: General");
+			System.out.println("Ticket rate =rs150");
 			break;
 			case 2:
-			System.out.println("Sleeper");
+			System.out.println("class :Sleeper");
+			System.out.println("Ticket rate = rs350");
 			break;
 			case 3:
-			System.out.println("AC 3 Tier");
+			System.out.println("class :AC 3 Tier");
+			System.out.println("Ticket rate = rs500");
 			break;
 			case 4:
-			System.out.println("AC 2 Tier");
+			System.out.println("class: AC 2 Tier");
+			System.out.println("Ticket rate = rs1000");
 			break;
 			case 5:
-			System.out.println("First Class");
+			System.out.println("class :First Class");
+			System.out.println("Ticket rate = rs900");
 			break;
 			default:
 			System.out.println("Thank you");
