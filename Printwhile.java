b2b3bae5510303f69527a91d6fda9abd7b618272
arunothayar-oss <arunothayar@gmail.com>
2025-12-31 12:15:34 +0530

    showing

---
+++ Printwhile.java
@@ -0,0 +1,12 @@
+class Printwhile
+{
+	public static void main(String[] args)
+	{
+		int i=1;
+		while(i<=15)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
---
+++ Reversewhile.java
@@ -0,0 +1,12 @@
+class Reversewhile
+{
+	public static void main(String[] args)
+	{
+		int i=10;
+		while(i>=1)
+		{
+			System.out.println(i);
+			i--;
+		}
+	}
+}
